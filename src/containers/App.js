d18df1a3f69d9238884a10e9aa5361a20b09c263
zabarullah <60830173+zabarullah@users.noreply.github.com>
2022-01-27 13:57:53 +0000

    adding files for github pages

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { connect } from "react-redux";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import './App.css';
 import ErrorBoundry from "../components/ErrorBoundry";
 
-function App() {
+import { requestRobots, setSearchField } from "../actions";
 
-    const [robots, setRobots] = useState([])
-    const [searchfield, setSearchfield] = useState('')
-    const [count, setCount] = useState(0)
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => { setRobots(users) });
-            console.log(count); // Count button (below) was created to test how useEffect works with dependency, preventing loops and re-rendering
-    },[count]) // count here is the dependency whereby if count changes then useEffect will run and render again each time
-
-    const onSearchChange = (event) => {
-        setSearchfield(event.target.value)
-    }
-
-    const filteredRobots = robots.filter(robot => {
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
-
-    if (robots.length === 0) {
-        return <h1> loading </h1>
-    } else {
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <button onClick={() => setCount(count+1)}> Click Me to see the count increase in the console</button>
-                <SearchBox searchChange={onSearchChange} />
-                <Scroll>
-                    <ErrorBoundry>
-                        <CardList robots={filteredRobots} />
-                    </ErrorBoundry>
-                </Scroll>
-            </div>
-        );
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+   return {
+       onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+       onRequestRobots: () => dispatch(requestRobots())
+   }
+}
+
+class App extends React.Component {
+
+    componentDidMount() {
+    this.props.onRequestRobots();
+    }
+
+    render() {
+        const { searchField, onSearchChange, robots, isPending } = this.props;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        })
+        if (isPending) {
+            return <h1> loading </h1>
+        } else {
+        return (
+            <div className='tc'>
+                <h1 className= 'f1'>RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
+                </Scroll>
+            </div>    
+        );
+        }
+    }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
